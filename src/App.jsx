4f997abcc30aad3324d56f23959456d38eb83189
water <wateruby8@gmail.com>
2026-04-05 22:11:05 +0800

    環境建立

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import axios from "axios";
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
+
+function App() {
+  return <></>;
+}
+
+export default App;
